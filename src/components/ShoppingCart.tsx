f9e1fb2e0386b342aa67b6dc6ffe71f2e5f33656
Emmanuel Sofuwa <emmanuelsofuwa2@gmail.com>
2022-08-23 10:44:27 +0100

    add files

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,38 @@
+import { Offcanvas, OffcanvasBody, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
+import CartItem from "./CartItem";
+import StoreItems from "../data/items.json";
+
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
+  const { closeCart, cartItems } = useShoppingCart();
+  return (
+    <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <OffcanvasBody>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = StoreItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
+        </Stack>
+      </OffcanvasBody>
+    </Offcanvas>
+  );
+};
+
+export default ShoppingCart;
